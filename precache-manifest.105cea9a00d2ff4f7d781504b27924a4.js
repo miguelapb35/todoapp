4ae51b3fc7f33eeff2eedb5fe4a62cd29e93637e
Miguel Angel <miguelapb35@gmail.com>
2019-12-28 18:35:34 -0500

    Updates

---
+++ precache-manifest.105cea9a00d2ff4f7d781504b27924a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb620d4ee4df3faa279170607ca0dfa",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "e4dac14854a7c1f9d586",
+    "url": "/todoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30b9a82db9aab372e0aa",
+    "url": "/todoapp/static/js/2.fa1c7be4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todoapp/static/js/2.fa1c7be4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4dac14854a7c1f9d586",
+    "url": "/todoapp/static/js/main.1237be70.chunk.js"
+  },
+  {
+    "revision": "77ba27e0b4b297640a85",
+    "url": "/todoapp/static/js/runtime-main.b2673753.js"
+  }
+]);
